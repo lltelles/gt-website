53f667b292fc2fc2ab88b4e86095119a89569cbc
Lucas Telles <lltellesss@gmail.com>
2023-09-10 12:23:41 -0300

    quem-somos page

--- server.js
+++ server.js
@@ -4,12 +4,8 @@ const path = require("path"); // Node.js built-in module for working with file p
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 3000
 const PORT = process.env.PORT || 3001;
 
-app.get("/home", (req, res) => {
-  const homePath = path.join(__dirname, "pages", "home.html"); // You can send any response you like here
-  res.sendFile(homePath);
-});
-
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "pages")));
 
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
@@ -17,5 +13,12 @@ app.listen(PORT, () => {
 
 // 👇 Start handling routes here
 
-const homeRoutes = require("./routes/home.routes");
-app.use("/home", homeRoutes);
+app.get("/home", (req, res) => {
+    const homePath = path.join(__dirname, "pages", "home.html"); // You can send any response you like here
+    res.sendFile(homePath);
+  });
+
+app.get('/quem-somos', (req, res) => {
+    const quemSomosPath = path.join(__dirname, 'pages', 'quem-somos.html')
+    res.sendFile(quemSomosPath)
+})
