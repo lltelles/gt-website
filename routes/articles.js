823ae8db3fa224a3ef85f1eabdb8716c4349846e
Lucas Telles <lltellesss@gmail.com>
2023-12-01 23:44:36 -0300

    Refactor articles.js: Improve error handling and code formatting

--- routes/articles.js
+++ routes/articles.js
@@ -41,17 +41,17 @@ router.put("/:id", async (req, res, next) => {
 
 function saveArticleAndRedirect(path) {
   return async (req, res) => {
-    let article = req.article
-      article.title = req.body.title
-      article.description = req.body.description
-      article.markdown = req.body.markdown
+    let article = req.article;
+    article.title = req.body.title;
+    article.description = req.body.description;
+    article.markdown = req.body.markdown;
     try {
       article = await article.save();
       res.redirect(`/articles/${article.slug}`);
     } catch (error) {
       console.error(error);
-      res.render(`articles/${path}`, { article: article });
+      res.status(500).render(`articles/${path}`, { article: article });
     }
-  }
+  };
 }
 module.exports = router;
